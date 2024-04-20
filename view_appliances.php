42cc36fc7d44e62868a5c4dae6baabe56fa53f2f
Bryant Hargreaves <hargreaves.bryant@gmail.com>
2024-04-20 15:13:08 -0500

    views

--- view_appliances.php
+++ view_appliances.php
@@ -2,22 +2,13 @@
 <html>
     <head>
         <title>View Appliances</title>
-        <style>
-            table {
-                border-collapse: collapse;
-                width: 100%;
-            }
-
-            th, td {
-                border: 1px solid black;
-                padding: 8px;
-                text-align: center;
-            }
-
-            th {
-                background-color: #f2f2f2;
-            }
-        </style>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet">
+        <!-- import index.css -->
+        <link rel="stylesheet" href="index.css">
     </head>
     <body>
         <h1>View Appliances</h1>
